1683982fd59492d58a5d9cb40766401d979883c5
LiubMok <liubik04@gmail.com>
2022-10-06 11:00:28 +0300

    Initial commit

---
+++ src/main/java/lotr/Character.java
@@ -0,0 +1,26 @@
+package lotr;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+
+@AllArgsConstructor
+public abstract class Character {
+    @Getter
+    protected int hp;
+    @Getter@Setter
+    protected int power;
+    public void setHp(int hp) {
+        this.hp = Math.max(hp, 0);
+    }
+    public boolean isAlive(){
+        return this.hp > 0;
+    }
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + "{" +
+                "hp=" + hp +
+                ", power=" + power +
+                '}';
+    }
+    public abstract void  kick(Character c);
+}
